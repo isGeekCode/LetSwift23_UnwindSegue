ab20b1731d3329d84123c2c4a8f50563334aa836
isGeekCode <ptuguitar@naver.com>
2023-10-04 21:26:57 +0900

    [ADD] Sample Project

---
+++ SampleProject/SampleProject/StoryBoardSystem/SegmentViewController.swift
@@ -0,0 +1,12 @@
+//
+//  SegmentViewController.swift
+//  test
+//
+//  Created by hyeonseok on 2023/10/02.
+//
+
+import UIKit
+
+class ViewControllerFirst: UIViewController { }
+class ViewControllerSecond: UIViewController { }
+class ViewControllerThird: UIViewController { }
